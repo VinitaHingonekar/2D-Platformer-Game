9f6c8e675d24e2018cdacf65376df2da921dbbfe
Vinita Hingonekar <hingonekarvinita@gmail.com>
2024-11-05 16:33:39 +0530

    Added Add Listener to the buttons

--- Assets/Scripts/LobbyController.cs
+++ Assets/Scripts/LobbyController.cs
@@ -2,9 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LobbyController : MonoBehaviour
 {
+    [SerializeField] private Button buttonPlay;
+    [SerializeField] private Button buttonQuit;
+    [SerializeField] private Button buttonLevels;
+
+    [SerializeField] private GameObject levelsMenu;
+    [SerializeField] private GameObject lobbyMenu;
+    
+    private void Awake( )
+    {
+        buttonPlay.onClick.AddListener(PlayGame);
+        buttonQuit.onClick.AddListener(QuitGame);
+        buttonLevels.onClick.AddListener(OpenLevelsMenu);
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Level 1");
@@ -12,7 +27,8 @@ public class LobbyController : MonoBehaviour
 
     public void OpenLevelsMenu()
     {
-
+        levelsMenu.SetActive(true);
+        lobbyMenu.SetActive(false);
     }
 
     public void QuitGame()
