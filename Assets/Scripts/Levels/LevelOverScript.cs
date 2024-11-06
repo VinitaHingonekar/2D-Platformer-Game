515776bc1cfb284996d4c63c5c6cc9bf7ba2bc4c
Vinita Hingonekar <hingonekarvinita@gmail.com>
2024-11-06 20:48:03 +0530

    Added locking unlocking feature to the levels

--- Assets/Scripts/Levels/LevelOverScript.cs
+++ Assets/Scripts/Levels/LevelOverScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelOverScript : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class LevelOverScript : MonoBehaviour
         if( other.gameObject.GetComponent<PlayerController>() != null)
         {
             Debug.Log("Next Level");
+            LevelManager.Instance.MarkCurrentLevelCompleted();
+            LevelManager.Instance.LoadNextLevel();            
         }
     }
 }
